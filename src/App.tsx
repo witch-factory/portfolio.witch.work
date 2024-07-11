a57ba48894f7dd9a20aaaf86511899144b894c30
witch-factory <soakdma37@gmail.com>
2024-07-11 12:34:28 +0900

    path alias 추가, reset css 설정, 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import { useState } from 'react';
-
-import viteLogo from '../public/vite.svg';
-
-import reactLogo from './assets/react.svg';
-import './App.css';
+import '@/styles/reset.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>안녕하세요</h1>
+        <p>하이</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount(c => c + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from "./App.tsx";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-	},
-	{
-		path: "/about",
-		element: <div>about</div>,
-	},
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/about',
+    element: <div>about</div>,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
