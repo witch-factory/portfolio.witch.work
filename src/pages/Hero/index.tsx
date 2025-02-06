c5714c95833d4c20a8b76082b70ed109aace4fac
witch-factory <soakdma37@gmail.com>
2025-02-06 17:35:44 +0900

    Hero 메인 페이지 추가

---
+++ src/pages/Hero/index.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/Layout';
+import IntroCard from '@/components/Intro';
+import FlyingWitch from '@/components/FlyingWitch';
+
+function Hero() {
+  return (
+    <Layout>
+      <FlyingWitch />
+      <IntroCard />
+    </Layout>
+  );
+}
+
+export default Hero;
