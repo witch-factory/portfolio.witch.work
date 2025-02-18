63f792881f58455f2f7fcc82b89af6ed281f144f
witch-factory <soakdma37@gmail.com>
2025-02-18 22:26:10 +0900

    별을 star 컴포넌트로 대체

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,15 @@
-import FlyingWitch from '../FlyingWitch';
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from '@/utils/constant';
+import Star from '../Star';
 import * as styles from './styles.css';
 
 const stars = Array.from({ length: 200 }, () => ({
-  x: Math.random() * 100,
-  y: Math.random() * 100,
+  x: Math.random() * SCREEN_WIDTH,
+  y: Math.random() * SCREEN_HEIGHT,
   twinkleDuration: Math.random() * 2 + 1.5,
 }));
 
+// TODO: night sky css 등을 치면 나오는 걸 참고해서 밤하늘을 더 잘 표현하기
+// TODO: 화면 리사이즈에 따라 새로 생긴 공간에도 별이 잘 나타나도록 수정하기
 function Layout({
   children,
 }: {
@@ -14,15 +17,9 @@ function Layout({
 }) {
   return (
     <article className={styles.background}>
-      <FlyingWitch />
-
       <div className={styles.starContainer}>
         {stars.map((star, index) => (
-          <div
-            key={index}
-            className={styles.star}
-            style={{ top: `${star.y}vh`, left: `${star.x}vw`, animationDuration: `${star.twinkleDuration}s` }}
-          />
+          <Star key={index} {...star} />
         ))}
       </div>
       {children}
