061c2719fe0780adb62b0dd521aaf76dc05741b1
witch-factory <soakdma37@gmail.com>
2025-05-15 20:49:10 +0900

    페이지 라우팅을 App.tsx로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
-import Hero from './pages/Hero';
+import Hero from './pages/Index';
 import { motion } from 'motion/react';
 import Header from './ui/Header';
 import Project from './pages/Project';
-import Interest from './pages/Interest';
+import Interest from './pages/Laboratory';
 import FlyingWitch from './components/FlyingWitch';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 // TODO: 이후에는 소개 - 활동 - 관심사 처럼 바꾸고 싶다.
 // 혹은 소개 - 프로젝트 - 활동 - 관심사
@@ -25,26 +26,22 @@ const pages = [
 
 // TODO: 페이지 전환 효과. 가령 밤하늘에서 별이 중앙으로 빨려들어가고 공간이 이동하는 등의 효과
 function App() {
-  const [activePageIndex, setActivePageIndex] = useState(0);
-
   return (
     <main className="w-screen h-screen overflow-hidden">
-      <Header
+      {/* <Header
         pageLabels={pages.map(page => page.label)}
         activePageIndex={activePageIndex}
         setActivePageIndex={setActivePageIndex}
-      />
+      /> */}
       {/* TODO: 마녀가 페이지 가장자리로 가면 스크롤이 생기는 문제 해결하기 */}
       {/* <FlyingWitch /> */}
-      <motion.div
-        className="flex flex-row"
-        animate={{ x: `-${activePageIndex * 100}vw` }}
-        transition={{ duration: 0.8, ease: 'easeInOut' }}
-      >
-        {pages.map((page, index) => (
-          <div key={index}>{page.component}</div>
-        ))}
-      </motion.div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<div>메인</div>} />
+          <Route path="/projects" element={<div>프로젝트</div>} />
+          <Route path="/laboratory" element={<div>관심사</div>} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import '@/styles/tailwind.css';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/about',
-    element: <div>about</div>,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 );
